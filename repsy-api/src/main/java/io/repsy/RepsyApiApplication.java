cc5a80b50b095196127ffe5c1471ff64c2df0dcb
AKIN AKSOY <akinaksoy@outlook.com.tr>
2025-04-22 15:22:47 +0300

    Initialize Repsy project structure with modules and dependencies
    Set up the project with a parent POM and three modules: `repsy-api`, `storage-fs-lib`, and `storage-object-lib`. Added necessary dependencies, build configurations, and IntelliJ project files. Included `.gitignore` and a basic Spring Boot application entry point.

---
+++ repsy-api/src/main/java/io/repsy/RepsyApiApplication.java
@@ -0,0 +1,12 @@
+package io.repsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepsyApiApplication.class, args);
+    }
+}
